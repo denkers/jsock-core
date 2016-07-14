047edacfe6181f547a1f81fed82e25805f38b736
denkers <kyle.business@inbox.com>
2016-07-15 00:37:10 +1200

    Removed unused server and client configs

--- jsock-core-server/src/main/java/com/kyleruss/jsockchat/server/core/ServerConfig.java
+++ jsock-core-server/src/main/java/com/kyleruss/jsockchat/server/core/ServerConfig.java
@@ -21,27 +21,4 @@ public class ServerConfig
     public static final int MESSAGE_SEND_SERVER_CODE        =   1;
     public static final int UPDATE_BROADCAST_SERVER_CODE    =   2;
     //---------------------------------------------------------
-    
-    
-    //---------------------------------------------------------
-    //  GUI CONSTS
-    //---------------------------------------------------------
-    public static final String WINDOW_TITLE         =   "JSockChat Server";
-    public static final int WINDOW_WIDTH            =   1000;
-    public static final int WINDOW_HEIGHT           =   600;
-    public static final String LOOKNFEEL_PACKAGE    =   "de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel";
-    public static final String IMAGES_DIR           =   "data/resources/";
-    public static final String AUTHOR_MESSAGE       =   "Kyle Russell\nAUT University\nwww.github.com/denkers";
-    public static final int MAX_LOG_CAPACITY        =   100;
-    //---------------------------------------------------------
-    
-    
-    //---------------------------------------------------------
-    //  DATABASE CONSTS
-    //---------------------------------------------------------
-    public static final String JDBC_CLASS   =   "org.sqlite.JDBC";
-    public static final String JDBC_DRIVER  =   "jdbc:sqlite:";
-    public static final String DB_FILE      =   "data/db/jsockchat.db";
-    public static final String CONN_URL     =   JDBC_DRIVER + DB_FILE;
-    //---------------------------------------------------------
 }
